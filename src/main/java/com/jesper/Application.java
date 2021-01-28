0ed90ad398a96a64490d6705816793555b028d26
czq <chuzhaoqi@aecg.com.cn>
2021-01-28 16:16:32 +0800

    删除不需要的类

--- src/main/java/com/jesper/Application.java
+++ src/main/java/com/jesper/Application.java
@@ -1,10 +1,7 @@
 package com.jesper;
 
-import com.jesper.util.RunnableThreadWebCount;
-import com.jesper.util.Timers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
